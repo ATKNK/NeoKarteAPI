72fcfa8b8e5e56f8804dbbcd76d42aa144a32cdd
Arthur Nakayama <arthurnakayama@gmail.com>
2026-04-19 01:22:10 -0300

    feat: created organization scaffold

---
+++ NeoKarteAPI/db/migrate/20260419042140_create_organizations.rb
@@ -0,0 +1,12 @@
+class CreateOrganizations < ActiveRecord::Migration[8.1]
+  def change
+    create_table :organizations do |t|
+      t.string :name
+      t.text :description
+      t.date :start
+      t.date :end
+
+      t.timestamps
+    end
+  end
+end
